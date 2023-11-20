a9b27fcbdc87da32e4520738cb156c0068a8a83c
Srinu007007 <98591537+Srinu007007@users.noreply.github.com>
2023-11-20 12:34:26 +0530

    Update App2.java

--- src/main/java/com/mycompany/app/App2.java
+++ src/main/java/com/mycompany/app/App2.java
@@ -8,6 +8,6 @@ public class App2
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!2" );
+        System.out.println( "Hello World!266666" );
     }
 }
